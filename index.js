136c7393269acef7f0a4f415efd5acd0c468e7c8
rajeevk47 <rajeevkumawat2007@gmail.com>
2023-12-07 19:42:25 +0530

    First commit --added Map , Player ,Collision blocks

--- index.js
+++ index.js
@@ -1,103 +1,179 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
+
+//========================================================//
+
 canvas.width = 1324
 canvas.height = 600
 c.fillStyle = 'white'
-c.fillRect(0,0, canvas.width,canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height)
+//------------//
+// function resizeCanvas() {
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
+// }
+// window.addEventListener("resize", resizeCanvas);
+// resizeCanvas();
 
-// ------------//
+//=========================================================//
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 160) {
+    collisionsMap.push(collisions.slice(i, 160 + i))
+}
+
+
+
+class Boundary {
+    static width = 24;
+    static height =24;
+    constructor({ position }) {
+        this.position = position
+        this.width = 24
+        this.height = 24
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+const offset = {
+    x: -810,
+    y : -900
+}
+
+const boundaries = []
+collisionsMap.forEach((row, i) => {
+    row.forEach((Symbol, j) => {
+        if(Symbol === 2227){
+        boundaries.push(new Boundary({
+            position: {
+                x: j * Boundary.width + offset.x,
+                y: i * Boundary.height + offset.y
+            }
+        })
+        )}
+    })
+})
+
+console.log(boundaries)
+
 const image = new Image()
 const playerImage = new Image()
-image.src = './img/bg400.png'
+
+image.src = './img/tiledrpg.png'
 playerImage.src = './img/playerDown.png'
 
-class Sprite{
-    constructor({position,velocity,image}){
+class Sprite {
+    constructor({ position, velocity, image }) {
         this.position = position
         this.image = image
     }
-    draw(){
-        c.drawImage(this.image,this.position.x,this.position.y)
+    drawi() {
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
-const background = new Sprite({position: { 
-    x:-590,
-    y:-560
-},
-  image: image
+const background = new Sprite({
+    position: {
+        x: offset.x,
+        y: offset.y
+    },
+    image: image
 })
 
-const keys ={
-    w:{
-        pressed : false
+const keys = {
+    w: {
+        pressed: false
     },
-    a:{
-        pressed : false
+    a: {
+        pressed: false
     },
-    s:{
-        pressed : false
+    s: {
+        pressed: false
     },
-    d:{
-        pressed : false
+    d: {
+        pressed: false
     }
-
 
 }
-function animate(){
+
+// function rectangularcollision({rectangle1,rectangle2}){
+//     return(
+//         rectangle1.position.x + rectangle1.>=rectangle2.position.x &&
+//         rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+//         rectangle1.position.y <=rectangle2.position.y + rectangle2.height &&
+//     )
+// }
+const test = new Boundary({
+    position:{
+        x:400,
+        y:400
+    }
+})
+
+function animate() {
     window.requestAnimationFrame(animate)
-    background.draw()
-    c.drawImage(playerImage,0,0,playerImage.width/4,playerImage.height,canvas.width/2 - (playerImage.width/4)/2,canvas.height/2-playerImage.height/2,playerImage.width/4,playerImage.height)
+    background.drawi()
+    // boundaries.forEach(boundary => {
+    //     boundary.draw()
+    // })
     
-    if(keys.w.pressed){
-        background.position.y =background.position.y + 7 
+    c.drawImage(playerImage, 0, 0, playerImage.width / 4, playerImage.height, canvas.width / 2 - (playerImage.width / 4) / 2, canvas.height / 2 - playerImage.height / 2, playerImage.width / 4, playerImage.height)
+
+    if (keys.w.pressed) {
+        background.position.y += 7
     }
-    else if(keys.a.pressed){
-        background.position.x = background.position.x +7
+    else if (keys.a.pressed) {
+        background.position.x += 7
     }
-    else if(keys.s.pressed){
-        background.position.y = background.position.y -7
+    else if (keys.s.pressed) {
+        background.position.y -= 7
     }
-    else if(keys.d.pressed){
-        background.position.x = background.position.x -7
+    else if (keys.d.pressed) {
+        background.position.x -= 7
     }
-    
+
 }
 
 animate()
-window.addEventListener('keydown',(e)=> {
-   // e= event
-    if(e.key == 'w' || e.key == 'W'){
-        keys.w.pressed =true
+window.addEventListener('keydown', (e) => {
+    // e= event
+    switch (e.key.toLowerCase()) {
+        case 'w':
+            keys.w.pressed = true
+            break
+        case 'a':
+            keys.a.pressed = true
+            break
+        case 's':
+            keys.s.pressed = true
+            break
+        case 'd':
+            keys.d.pressed = true
+            break
     }
-    else if(e.key == 'a' || e.key == 'A'){
-        keys.a.pressed =true
-           }
-    else if(e.key == 's' || e.key == 'S'){
-        keys.s.pressed =true
-        }
-    else if(e.key == 'd' || e.key == 'D'){
-        keys.d.pressed =true
-        }
 }
 )
 
 window.addEventListener('keyup', (e) => {
-    if(e.key == 'w' || e.key == 'W'){
-        keys.w.pressed =false
+    // e= event
+    switch (e.key.toLowerCase()) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
     }
-    else if(e.key == 'a' || e.key == 'A'){
-        keys.a.pressed =false
-           }
-    else if(e.key == 's' || e.key == 'S'){
-        keys.s.pressed =false
-        }
-    else if(e.key == 'd' || e.key == 'D'){
-        keys.d.pressed =false
-        }
-        //May use switch case
 }
-      
 )
- 
+
+
+
 //----------//
